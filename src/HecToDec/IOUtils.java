76901e4ccfe4779bb4c18d58c50c4f96e0f17082
luvnjoae <lichang0637@163.com>
2019-09-04 21:56:59 +0800

    add

---
+++ src/HecToDec/IOUtils.java
@@ -0,0 +1,59 @@
+package HecToDec;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class IOUtils {
+    /**
+     * 读取txt文件，并以Arraylist<String>形式返回
+     * @param path txt路径
+     * @return list（String）
+     */
+    public ArrayList<String> readTxt(String path) {
+        try(
+                FileReader reader = new FileReader(path);
+                BufferedReader br = new BufferedReader(reader)
+        ) {
+            String line;
+            ArrayList<String> list = new ArrayList<>();
+
+            while ((line = br.readLine()) != null) {
+//                System.out.println(line);
+                list.add(line);
+            }
+
+            return list;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    /**
+     * 写入txt文件
+     * @param path 存储路径
+     * @param contents 写入String[]形式的数据
+     */
+    public void writeTxt(String path, String[] contents) {
+        try (
+                FileWriter writer = new FileWriter(path);
+                BufferedWriter bw = new BufferedWriter(writer)
+        ) {
+            for (String content : contents) {
+                bw.write(content + "\r\n");
+            }
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readJson(String path){
+        //暂时不需要
+    }
+
+    public void writeJson(String path){
+        //暂时不需要
+    }
+}
--- src/HecToDec/demo.java
+++ src/HecToDec/demo.java
@@ -1,9 +1,36 @@
 package HecToDec;
 
+import java.util.ArrayList;
+
 public class demo {
     public static void main(String[] args) {
+        // 设置源文件路径 与 目标文件路径
+        String sourcePath = "resource/Data/test.txt";
+        String targetPath = "resource/Data/target.txt";
+        // 加载读取与写入工具类
+        IOUtils IOUtils = new IOUtils();
+        // 向目标文件中写入 表头 （序号、电压、电流、送丝速度）
+        String[] strings = {"Seq\tVoltage\tCurrent\tWire_Feed_Speed"};
+        IOUtils.writeTxt(targetPath, strings);
+        // 读取源文件 并 转换
+        ArrayList<String> sourceList = IOUtils.readTxt(sourcePath);  // 接收读取的txt内容（字符串形式）
+        StringBuilder Voltage;  // 存储电压的高位和低位（字符串十六进制）
+        StringBuilder Current;  // 存储电流的高位和低位（字符串十六进制）
+        StringBuilder Wire_Feed_Speed;  // 存储送丝速度的高位和低位（字符串十六进制）
+
+        for (int i = 0; i < sourceList.size(); i++) {
+            String[] strs = sourceList.get(i).split(" ");
+            // 转存电压
+//            Voltage = strs;
+
+        }
+//
+//        for (Integer[] integers : TargetList) {
+//            for (Integer integer : integers) {
+//                System.out.print(integer + " ");
+//            }
+//            System.out.println();
+//        }
 
     }
-
-
 }
